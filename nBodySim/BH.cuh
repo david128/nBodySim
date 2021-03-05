ae022038b351af8fbedd85b23d9016fcc0843027
David <davidireland128@gmail.com>
2021-03-05 17:19:39 +0000

    uncommented code

--- nBodySim/BH.cuh
+++ nBodySim/BH.cuh
@@ -1,15 +1,15 @@
-﻿//#include "BHTree.h"
-//#include "cuda_runtime.h"
-//#include "cuda.h"
-//#include "device_launch_parameters.h"
-//
-//class BHParallelTree : BHTree
-//{
-//public:
-//	void ConstructTree(std::vector<Particle*>* particles);
-//	void SplitOnce();
-//	//void SplitNodeInP(Node* currentNode);
-//
-//};
-//
-//
+﻿#include "BHTree.h"
+#include "cuda_runtime.h"
+#include "cuda.h"
+#include "device_launch_parameters.h"
+
+class BHParallelTree : BHTree
+{
+public:
+	void ConstructTree(std::vector<Particle*>* particles);
+	void SplitOnce();
+	//void SplitNodeInP(Node* currentNode);
+
+};
+
+
